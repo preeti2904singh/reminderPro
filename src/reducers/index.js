e52222ce418dc1f6456787b51c6b7cfbe092941f
Preeti <preeti@IL323.local>
2018-02-05 17:19:53 +0530

    moment added and delete functionality implemented

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,18 @@
-import {ADD_REMINDER} from '../constants';
+import {ADD_REMINDER, DELETE_REMINDER} from '../constants';
 
 const reminder = (action) => {
+    let {text, dueDate} = action;
     return {
-        text: action.text,
-        id: Math.random()
+        id: Math.random(),
+        text,
+        dueDate
     }
+}
+
+const removeById = (state = [], id) => {
+    const reminders = state.filter(reminder => reminder.id !== id);
+    console.log('new reduced reminders', reminders);
+    return reminders;
 }
 
 const reminders = (state = [], action) => {
@@ -12,6 +20,9 @@ const reminders = (state = [], action) => {
     switch(action.type) {
         case ADD_REMINDER:
             reminders = [...state, reminder(action)];
+            return reminders;
+        case DELETE_REMINDER:
+            reminders = removeById(state, action.id);
             return reminders;
         default: return state;
     }
